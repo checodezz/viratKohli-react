9ec6942a0373d379cbfcdeb60ed01d97327751ea
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-09 07:25:59 +0000

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer"
 export default function App() {
   return (
     <>
       <Header />
       <main>React ⚛️ + Vite ⚡ + Replit</main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light py-3">
+      <div className="container">
+        <p>&copy; {new Date().getFullYear()} Virat Kohli Fan Club</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
