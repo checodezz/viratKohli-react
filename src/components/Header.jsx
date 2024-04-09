7a64a086a9c290db41157c1183aece12e778f429
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-09 06:31:24 +0000

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Virat Kohli</h1>
+      <ul>
+        <li>Overview</li>
+        <li>Biography</li>
+        <li>Records</li>
+      </ul>
+    </header>
+  );
+};
